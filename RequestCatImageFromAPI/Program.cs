73b6f127adf7934749b452d06bd3a6d45b65ab0f
Martin Terziev <m.terziev08@abv.bg>
2025-07-04 22:04:09 +0300

    Added HTTP request to API

--- RequestCatImageFromAPI/Program.cs
+++ RequestCatImageFromAPI/Program.cs
@@ -1,14 +1,38 @@
 ﻿using static System.Net.WebRequestMethods;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RequestCatImageFromAPI
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            using (var client = new HttpClient())
+            {
+                // Setting up HTTP request to API
+
+                var apiURL = new Uri("https://aws.random.cat/meow");
+
+                Console.WriteLine("Fetching cat image from API...");
+                var requestResult = await client.GetAsync(apiURL);
+
+                try
+                {
+                    requestResult.EnsureSuccessStatusCode();
+                    Console.WriteLine("Image fetched successfully!");
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+
+                // Deserializing response
+                var requestResponse =  await requestResult.Content.ReadAsStringAsync();
+
+            }
         }
     }
 }
