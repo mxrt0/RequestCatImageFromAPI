ba827749d85f5da4ac296c352a4ee61c2600e11c
Martin Terziev <m.terziev08@abv.bg>
2025-07-04 21:58:06 +0300

    Add project files.

---
+++ RequestCatImageFromAPI/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Net.WebRequestMethods;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace RequestCatImageFromAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
